d2cb47ed1cc80b3360215b6dbce934e3b44e877e
antwan.momtaz.ext <antwan.momtaz.ext@siemens.com>
2024-09-08 10:31:24 -0500

    edited fuzzer build file

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,6 +1,6 @@
 make -j$(nproc) all    # Build the fuzz targets.
 
 # Copy the fuzzer executables, zip-ed corpora, option and dictionary files to $OUT
-find . -name '*_fuzzer' -exec cp -v '{}' $OUT ';'
-find . -name '*_fuzzer.dict' -exec cp -v '{}' $OUT ';'     # If you have dictionaries.
-find . -name '*_fuzzer.options' -exec cp -v '{}' $OUT ';'  # If you have custom options.
+find . -name '*fuzzer' -exec cp -v '{}' $OUT ';'
+find . -name '*fuzzer.dict' -exec cp -v '{}' $OUT ';'     # If you have dictionaries.
+find . -name '*fuzzer.options' -exec cp -v '{}' $OUT ';'  # If you have custom options.
